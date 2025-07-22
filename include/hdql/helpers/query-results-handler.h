4662c3d2149678f7f6df12954027b9a10a3e2fbd
Renat R. Dusaev <crank@qcrypt.org>
2025-07-22 17:13:15 +0700

    ~ drafting new api for query results handling

--- include/hdql/helpers/query-results-handler.h
+++ include/hdql/helpers/query-results-handler.h
@@ -12,32 +12,36 @@ extern "C" {
 
 struct hdql_QueryResultsWorkspace;  /* opaque */
 
-/**\brief Query results receiver assuming tabular logic
+/**\brief Query results receiver interface
  *
  * This helper interface is meant to simplify integration with query object
- * defining a rule to populate table with query results. The query always
- * evaluates to scalar instance, producing a series
+ * defining a rule to populate tables and objects with query results.
+ *
+ * The query always evaluates to scalar instance, producing a series
  * of ((key1, key22, ...), value) pair every time it is advanced. In the
  * simplest case the value is atomic scalar value (of arithmetic or string
  * type). More elaborated and very important scenario is when the value is
  * of (virtual) compound type to which user would like to apply sub-queries.
+ * Handling code might also be interested in handling collections in some way.
+ *
  * For instance, populating 2D histogram would require obtaining two random
- * joint variables --- which is the subject of two (possibly, very simple)
+ * joint variables -- which is the subject of two (possibly, very simple)
  * sub-queries to root query result.
  *
+ * This interface and related functions provide a helepr API with some
+ * boilerplate implementation to handle query results, possibly recursively.
  * */
 struct hdql_iQueryResultsHandler {
     /** Arbitrary user pointer forwarded into all interface's calls */
     void * userdata;
 
-    /**\brief Handles query result's type (attr.definition)
-     *
-     * Interface implementation uses it to prepare streaming schema as
-     * provided "attribute definition" brings all the introspeciton information
-     * about query result types.
-     *
-     * Called in `hdql_query_result_table_init()`. */
-    int (*handle_result_type)(const struct hdql_AttrDef *, void *);
+    void * (*init_atomic_ad)(const struct hdql_AttrDef *, const struct hdql_ValueInterface *, void * rh);
+    void * (*init_compound_ad)(const struct hdql_AttrDef *, const struct hdql_Compound * c, void * rh);
+    void * (*init_collection_ad)(const struct hdql_AttrDef *, void * rh);
+
+    int (*handle_atomic)(hdql_Datum_t, void * ah, void * rh);
+    int (*handle_compound)(hdql_Datum_t, void * ah, void * rh);
+    int (*handle_collection)(hdql_Datum_t, void * ah, void * rh);
 
     /**\brief Callback to handle keys definition
      *
@@ -55,11 +59,23 @@ struct hdql_iQueryResultsHandler {
      * attributes and keys definition.
      *
      * Called in `hdql_query_result_table_init()` when not null. */
-    int (*finalize_schema)(void * userdata);
+    int (*finalize_schema)(void * rh);
 
     int (*handle_record)(hdql_Datum_t, void *);
 };
 
+/**\brief Initializes handler based on the query object and given interface
+ *
+ * 1. Retrieves top attribute definition (returned type), then depending on
+ *    the query result:
+ *      - if `handle_atomic_scalar()` is defined by iface and query result type
+ *        is atomic, allocates scalar handle
+ *      - if `handle_compound()` is defined by iface and query result type is
+ *        compound, allocates compound handle
+ * 2. If `handle_key_views()` is set, alloates keys for query and their flat
+ *    views, forwards call to `handle_keys()`
+ * 3. Calls `finalize_schema()` if defined.
+ * */
 struct hdql_QueryResultsWorkspace *
 hdql_query_results_init(
           struct hdql_Query * q
--- src/helpers/query-results-handler.c
+++ src/helpers/query-results-handler.c
@@ -11,17 +11,50 @@
 #include <stdlib.h>
 #include <assert.h>
 
+struct hdql_CSVHandler;  /* fwd */
+struct hdql_AttrHandler;  /* fwd */
+struct hdql_AttrHandlerTier;  /* fwd */
+
+/* recursive struct defining one level attribute handlers */
+struct hdql_AttrHandlerTier {
+    size_t n;  /* number handlers at this level */
+    struct hdql_AttrHandler ** handlers;
+};
+
+/* Handles individual attributes in query result. May expand to multiple
+ * columns (e.g. for scalar compound attributes) */
+struct hdql_AttrHandler {
+    const char * name;
+    /* Attribute entry definition */
+    const struct hdql_AttrDef * ad;
+
+    /* Internal callback, dispatchng calls, related to this node */
+    void (*value_handler)( struct hdql_CSVHandler *, hdql_Datum_t, struct hdql_AttrHandler * ad);
+
+    /* runtime short-lived state, used to dereference scalar or collection
+     * attributes (depends on iface) */
+    union {
+        hdql_Datum_t scSupp;
+        hdql_It_t collectionIt;
+    } dynamicData;
+
+    union {
+        void * atomicAttrData;
+        /* child tiers to stringify compounds */
+        struct hdql_AttrHandlerTier attrHandlers;
+    } payload;
+};
+
 struct hdql_QueryResultsWorkspace {
     struct hdql_Query * q;
     struct hdql_Context * ctx;
     struct hdql_iQueryResultsHandler * iqr;
 
-    /* Keys pointer */
+    struct hdql_AttrHandler rootObjectHandler;
+
     struct hdql_CollectionKey * keys;
-    /* Number of key flat views */
+    struct hdql_KeyView * flatKeyViews;
     size_t flatKeyViewLength;
-    /* Keys flat views array */
-    struct hdql_KeyView * kv;
 };
 
 /* Allocates structure to track keys with their flattened views
@@ -37,16 +70,80 @@ _query_result_table_init_keys( struct hdql_QueryResultsWorkspace * ws ) {
     int rc = hdql_query_keys_reserve(ws->q, &ws->keys, ws->ctx);
     assert(rc == 0);
     ws->flatKeyViewLength = hdql_keys_flat_view_size(ws->keys, ws->ctx);
-    ws->kv = ws->flatKeyViewLength
+    ws->flatKeyViews = ws->flatKeyViewLength
                       ? (struct hdql_KeyView *) malloc(sizeof(struct hdql_KeyView)*ws->flatKeyViewLength)
                       : NULL;
-    hdql_keys_flat_view_update(ws->q, ws->keys, ws->kv, ws->ctx);
+    hdql_keys_flat_view_update(ws->q, ws->keys, ws->flatKeyViews, ws->ctx);
     assert(ws->iqr->handle_keys);
-    return ws->iqr->handle_keys( ws->keys, ws->kv, ws->flatKeyViewLength
+    return ws->iqr->handle_keys(
+              ws->keys
+            , ws->flatKeyViews
+            , ws->flatKeyViewLength
             , ws->iqr->userdata);
 }
 
+/*
+ * init
+ */
 
+static void _handle_collection_as_entry(      struct hdql_CSVHandler * csv, hdql_Datum_t ownerDatum, struct hdql_AttrHandler * ad);
+static void _handle_scalar_compound_as_entry( struct hdql_CSVHandler * csv, hdql_Datum_t ownerDatum, struct hdql_AttrHandler * ad);
+static void _handle_scalar_atomic_as_entry(   struct hdql_CSVHandler * csv, hdql_Datum_t ownerDatum, struct hdql_AttrHandler * ad);
+static void _handle_scalar_atomic_value_as_entry(   struct hdql_CSVHandler * csv, hdql_Datum_t ownerDatum, struct hdql_AttrHandler * ad);
+
+static int
+_results_handler_set_result_type( const struct hdql_AttrDef * ad
+            , struct hdql_QueryResultsWorkspace * ws
+            ) {
+
+    if(ws->rootObjectHandler.ad != NULL) {
+        /* make sure it hasn't being called before */
+        return -1;  // TODO: dedicated EC
+    }
+    ws->rootObjectHandler.ad = ad;
+    /* Special case here is when root type is a collection. Contrary to
+     * in-depth iteration of the query result (assigned
+     * by _assign_value_handler(), we expect query itslef to iterate over this
+     * collection, so we must consider datum as a scalar item here. */
+    if(hdql_attr_def_is_atomic(ad)) {
+        struct hdql_ValueTypes * valTypes = hdql_context_get_types(ws->ctx);
+        assert(valTypes);
+        const struct hdql_ValueInterface * vi
+                = hdql_types_get_type(valTypes, hdql_attr_def_get_atomic_value_type_code(ad));
+
+        ws->rootObjectHandler.value_handler = _handle_scalar_atomic_as_entry;
+        if(hdql_attr_def_is_scalar(ad)) {
+            if(ws->iqr->init_atomic_ad) {
+                ws->rootObjectHandler.payload.atomicAttrData = ws->iqr->init_atomic_ad(ad, vi, ws->iqr->userdata);
+                /* TODO: check result? */
+            }
+        } else {
+            assert(hdql_attr_def_is_collection(ad));
+            /* This is a special (and simplest possible) case when query
+             * yields a series of scalars that this handler must not iterate
+             * by itself. Just use this datum to stringify as is. */
+            if(ws->iqr->init_atomic_ad) {
+                ws->rootObjectHandler.payload.atomicAttrData = ws->iqr->init_atomic_ad(ad, vi, ws->iqr->userdata);
+                /* TODO: check result? */
+            }
+            ws->rootObjectHandler.value_handler = _handle_scalar_atomic_value_as_entry;
+        }
+    } else if(hdql_attr_def_is_compound(ad)) {
+
+    }
+    #ifndef DNDEBUG
+    else {
+        char bf[128];
+        hdql_top_attr_str(ad, bf, sizeof(bf), ws->ctx);
+        fputs("DEBUG ASSERTION FAILED: logic error: can't handle top level attr def of query result"
+                " yielding type `", stderr);
+        fputs(bf, stderr);
+        fputs("'.\n", stderr);
+        assert(0);
+    }
+    #endif
+    return 0;
+}
 
 struct hdql_QueryResultsWorkspace *
 hdql_query_results_init(
@@ -72,15 +169,14 @@ hdql_query_results_init(
     ws->iqr = iqr;
 
     /* if attribute handling is enabled in iface implem, process attributes */
-    if(iqr->handle_result_type) {
-        const struct hdql_AttrDef * ad = hdql_query_top_attr(q);
-        assert(ad);
-        if(0 != (rc = iqr->handle_result_type(ad, iqr->userdata))) {
-            /* TODO communicate error other way */
-            fprintf(stderr, "Can't process attributes of query result: %d\n", rc);
-            return NULL;
-        }
+    const struct hdql_AttrDef * ad = hdql_query_top_attr(q);
+    assert(ad);
+    if(0 != (rc = _results_handler_set_result_type(ad, ws))) {
+        /* TODO communicate error other way */
+        fprintf(stderr, "Can't process attributes of query result: %d\n", rc);
+        return NULL;
     }
+
     if(iqr->handle_keys) {
         if(0 != (rc = _query_result_table_init_keys(ws))) {
             /* TODO communicate error other way */
@@ -90,7 +186,7 @@ hdql_query_results_init(
     } else {
         ws->flatKeyViewLength = 0;
         ws->keys = NULL;
-        ws->kv = NULL;
+        ws->flatKeyViews = NULL;
     }
     if(iqr->finalize_schema) iqr->finalize_schema(iqr->userdata);
 
@@ -115,7 +211,7 @@ hdql_query_results_process_records_from( struct hdql_Datum * d
 
 int
 hdql_query_results_destroy( struct hdql_QueryResultsWorkspace * ws ) {
-    if(ws->kv) free(ws->kv);
+    if(ws->flatKeyViews) free(ws->flatKeyViews);
     if(ws->keys) hdql_query_keys_destroy(ws->keys, ws->ctx);
     hdql_context_free(ws->ctx, (hdql_Datum_t) ws);
     return 0;

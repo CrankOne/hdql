6db0879149b02d415402e985ecd99095c90f48d1
Renat Dusaev <crank@qcrypt.org>
2026-06-03 11:29:49 +0700

    value implem: continuing drafting pure C impl

--- src/value.c
+++ src/value.c
@@ -1,8 +1,10 @@
 #include "hdql/value.h"
 #include "hdql/errors.h"
+#include "hdql/util/avl-tree.h"
 #include "hdql/util/ht.h"
 
 #include <assert.h>
+#include <string.h>
 
 #ifndef HDQL_TIER_BITSIZE
 /**\brief Defines number of bits used to store the type table's tier */
@@ -27,12 +29,23 @@ _get_tier_number(hdql_ValueTypeCode_t code) {
     return (code & gTierMask) >> (HDQL_VALUE_TYPEDEF_CODE_BITSIZE - HDQL_TIER_BITSIZE);
 }
 
+/* Internaly represents table: (code:hdql_ValueTypeCode_t, name:char*, typeDef:hdql_ValueInterface*)
+ *
+ * CAVEAT: a little hacky -- codes are stored in has table as integers
+ * written in pointer field (not pointer referencing number).
+ * */
 struct hdql_ValueTypes {
     struct hdql_ValueTypes *parent;
+    /* code by name hash table
+     * NOTE: code number is stored as pointer; never try to de-reference it! */
     struct hdql_ht *codeByName;
-    struct hdql_avl *itemByCode;
+    /* value interface by code */
+    struct hdql_umap *itemByCode;
+    /* current types tier, root is 0, max is 32 (4 bits) */
     uint8_t tier;
+    /* type promotions table; invalidated on type insertion */
     struct hdql_ArithTypePromotionTable *promotions;
+    /* flag to track type promotions table validity */
     bool isPromotionsValid;
 };  /* struct hdql_ValueTypes */
 
@@ -45,20 +58,46 @@ hdql_types_define( struct hdql_ValueTypes * vt
     if(NULL == vti->name || *vti->name == '\0') {
         return HDQL_ERR_BAD_ARGUMENT;
     }
-    size_t nItems = hdql_ht_size(vt->itemByCode);
+    size_t nItems = hdql_umap_size(vt->itemByCode);
     if(nItems >= gMaxTypeIDInTier) {
         return HDQL_ERR_OPERATION_NOT_SUPPORTED;  /* IDs exceed for tier */
     }
+
+    /* generate type code entry */
     hdql_ValueTypeCode_t tCode = (hdql_ValueTypeCode_t) nItems;
     tCode |= ((hdql_ValueTypeCode_t) vt->tier) << (HDQL_VALUE_TYPEDEF_CODE_BITSIZE - HDQL_TIER_BITSIZE);
-    auto ir = _codeByName.emplace(iface.name, tCode);
-    if(!ir.second) return -3;  // duplicating name
-    auto iir = _itemByCode.emplace(tCode, iface);
-    if(!iir.second) {
-        _codeByName.erase(ir.first);
-        return -4;  // internal logic error
+    /* pre-caution -- ensure the code is unique */
+    void *byCodeEntry = hdql_umap_get(vt->itemByCode, tCode);
+    if(byCodeEntry)
+        return HDQL_ERR_GENERIC;  /* internal inconsistency: generated code exists */
+
+    /* add name-to-code entry */
+    const size_t nameLen1 = strlen(vti->name) + 1;
+    size_t nb; struct hdql_htEntry * entry; {
+        entry = hdql_ht_lookup(vt->codeByName, (const unsigned char *) vti->name
+                    , nameLen1, &nb);
+        if(entry) return HDQL_ERR_NAME_COLLISION;  /* Type name exists in current context */
+        hdql_ht_ins_cached(vt->codeByName
+                , (const unsigned char *) vti->name, nameLen1
+                , (void *) ((unsigned long) tCode), entry, nb);
     }
-    return 0;
+
+    /* add code-to-type record */
+    int rc = hdql_umap_insert(vt->itemByCode, tCode, (void *) vti);
+    int myRC = HDQL_ERR_CODE_OK;
+    switch(rc) {
+        case HDQL_AVL_OK:
+            return myRC;
+
+        case HDQL_AVL_MEM_ERROR:
+            myRC = HDQL_ERR_MEMORY;
+        case HDQL_AVL_BAD_ARG_ERROR:
+            myRC = HDQL_ERR_BAD_ARGUMENT;
+        case HDQL_AVL_CHANGED:
+            myRC = HDQL_ERR_GENERIC;
+        default:
+            hdql_ht_remove(vt->codeByName, (const unsigned char *) vti->name, nameLen1);
+            return myRC;
 }
 
 int
